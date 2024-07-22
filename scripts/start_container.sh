dd09108c8203ca4988ce428053f22a1f90cc54d1
D-Jamil <98935683+D-Jamil@users.noreply.github.com>
2024-07-22 20:55:44 +0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -6,3 +6,5 @@ docker pull docker push rabiadanish/simple-python-flask-app:latest
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 rabiadanish/simple-python-flask-app:latest
+
+echo "container started successfully"
